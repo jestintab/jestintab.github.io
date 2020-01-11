fa78b37723d537e73005736161fbd29752913ed2
IT Aljedad <jestintab@gmail.com>
2020-01-11 16:57:54 +0300

    Portfolio ver1

---
+++ build/precache-manifest.7a78c7fc8209c00aa9aca5d10e94b530.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851a9bf3c2db83255c25717687996205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0d86d43ace3872900f8",
+    "url": "/static/css/main.52c7bcbc.chunk.css"
+  },
+  {
+    "revision": "f770bc4ca9c893ab0abb",
+    "url": "/static/js/2.21aaae71.chunk.js"
+  },
+  {
+    "revision": "d0d86d43ace3872900f8",
+    "url": "/static/js/main.1774db65.chunk.js"
+  },
+  {
+    "revision": "d7124f41387d88c92f3f",
+    "url": "/static/js/runtime-main.30d7367c.js"
+  }
+]);
